378e46b7af85345d0876eb5e5e395c7fd4bce36b
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-07-08 04:26:22 -0300

    Criação da Base de Template HTML

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse("Vocês está na página de cadastro")
+    return render(request, 'cadastro.html')
 
 def logar(request):
     return HttpResponse("Você está na página de Login")
--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autenticacao',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'nutri_lab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
