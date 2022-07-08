67312c22f4fe887f5f48a59af51857853c88d099
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-07-08 05:37:21 -0300

    Posterior a validação de dados

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        return HttpResponse(f'Usuario: {usuario} \n Email: {email} Senha: {senha}')
 
 def logar(request):
     return HttpResponse("Você está na página de Login")
