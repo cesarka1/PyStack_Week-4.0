06c1060193e61679498ec0c379a6628935d3aed5
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-07-09 23:46:27 -0300

    Cadastro de Paciente
    *Modal para preenchimento
    *Validações dos dados preenchidos

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages import constants
+
 
 @login_required(login_url='/auth/login')
 def pacientes (request):
-    return render(request, 'paciente.html')
+    if request.method == "GET":
+        return render(request, 'paciente.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sexo = request.POST.get('sexo')
+        idade = request.POST.get('idade')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        if (len(nome.strip()) == 0) or (len(sexo.strip()) == 0) or (len(idade.strip()) == 0) or (len(email.strip()) == 0) or (len(telefone.strip()) == 0):
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/pacientes/')
+
+        if not idade.isnumeric():
+            messages.add_message(request, constants.ERROR, 'Digite uma idade válida')
+            return redirect('/pacientes/')
+
+        pacientes = pacientes.objects.filter(email=email)
+
+        if pacientes.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um paciente com esse E-mail')
+            return redirect('/pacientes/')
+
+        return HttpResponse(f"{nome}, {sexo}, {idade}, {email}, {telefone}")
